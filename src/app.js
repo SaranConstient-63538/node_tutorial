b9a1b9158705fe02434f991814fb18de44f2fcac
SaranConstient-63538 <sarankumar@constient.com>
2022-09-14 10:29:39 +0530

    updated the connectvity in mysql

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ db.connect((err)=>{
 //create a database
 app.get('/createdb',(req,res)=>{
     let sql = 'CREATE DATABASE nodemysql';
-    db.query(sql, (err, result)=>{
+    let query = db.query(sql, (err, result)=>{
         if(err) throw err;
         console.log('db is created')
         res.send('Database is Created')
@@ -27,7 +27,7 @@ app.get('/createdb',(req,res)=>{
 //create a table
 app.get('/createtable',(req,res)=>{
     let sql =  "CREATE TABLE samplepost(id int AUTO_INCREMENT, title VARCHAR(255),body VARCHAR(255),PRIMARY KEY(id))";
-    db.query(sql,(err,result)=>{
+    let query = db.query(sql,(err,result)=>{
         if(err) throw err;
         console.log('created table')
         res.send('Table is Created')
@@ -37,22 +37,44 @@ app.get('/createtable',(req,res)=>{
 app.get('/inserttable', (req,res)=>{
     let post = {title:'Post One', body:'This is a body 1'}
     let sql ='INSERT  INTO samplepost SET ?'
-    db.query(sql,post,(err,result)=>{
+    let query =  db.query(sql,post,(err,result)=>{
         if(err) throw err;
-        console.log('one row is inserted')
+        console.log('One row is inserted')
         res.send('One Row is Created')
     })
 })
-app.get('/inserttable', (req,res)=>{
-    let post = {title:'Post One 2', body:'This is a body 2'}
+app.get('/inserttable1', (req,res)=>{
+    let post = {title:'Post Two', body:'This is a body 2'}
     let sql ='INSERT  INTO samplepost SET ?'
-    db.query(sql,post,(err,result)=>{
+    let query = db.query(sql,post,(err,result)=>{
         if(err) throw err;
-        console.log('one row is inserted')
-        res.send('One Row is Created')
+        console.log('Two row is inserted')
+        res.send('Two Row is Created')
     })
 })
-
+//view the table with rows
+app.get('/getsamplepost', (req,res)=>{
+    let sql = 'SELECT * FROM samplepost'
+    let query = db.query(sql, (err,result)=>{
+        if(err) throw err
+        console.log(result)
+        res.send('view the table')
+    })
+})
+//view the table with single row
+app.get('/getsamplepost/:id', (req,res)=>{
+    let sql = `SELECT * FROM samplepost where id=${req.params.id}`
+    let query = db.query(sql, (err,result)=>{
+        if(err) throw err
+        console.log(result)
+        res.send('view the table with single row')
+    })
+})
+//edit the table
+app.get('/getsamplepost/:id',(req,res)=>{
+    let newTitle = 'updated title'
+    let sql = `UPDATE samplepost set `
+})
 app.listen('3000',()=>{
     console.log('Server is successfully running on port 3000')
 })
